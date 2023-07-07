49ec3fa3317cee54535ccbd6729c7e9f3e695640
Ayush <ayushmahobia01@gmail.com>
2023-07-07 20:26:19 +0530

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import userTask from "./routes/task.js"
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors"
 
 export const app = express(); // creating server
 
@@ -14,6 +15,14 @@ config({
 //Using Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
+
 //Using routes
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/task', userTask)
